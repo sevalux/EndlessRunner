b5dec3471daee634a331bbb0b980bfeded4f8b7b
Josh Beyer <mydevaccs@gmail.com>
2018-06-10 00:57:45 -0500

    test

---
+++ Endless Runner Project/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Start something
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Update something
+		
+	}
+}

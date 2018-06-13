40661b633654a40be9538c4718b4e2e54e14bc0e
Josh Beyer <mydevaccs@gmail.com>
2018-06-12 19:22:39 -0500

    Fixed inverse issue with camera

--- Endless Runner Project/Assets/Scripts/CameraController.cs
+++ Endless Runner Project/Assets/Scripts/CameraController.cs
@@ -23,7 +23,7 @@ public class CameraController : MonoBehaviour {
 	private void Update() {
 		Vector3 offsetPosition = new Vector3(target.position.x + currentLane(), target.position.y, target.position.z);
 		Vector3 targetCamPos = offsetPosition + offset;
-		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing);
 
 		transform.LookAt(target);
 	}
@@ -33,9 +33,9 @@ public class CameraController : MonoBehaviour {
 
 		// 0,3f is extra room incase of 0.00123123 value on x axis
 		if(target.position.x < -0.3f){
-			lookOffset = -xOffset;
-		} else if (target.position.x > 0.3f){
 			lookOffset = xOffset;
+		} else if (target.position.x > 0.3f){
+			lookOffset = -xOffset;
 		} else {
 			lookOffset = 0;
 		}
